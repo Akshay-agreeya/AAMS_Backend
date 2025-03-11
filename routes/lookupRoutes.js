e55f452eced8810b6ad59c498863cfd8cd07eddb
Priya Varshney <priya.varshney@agreeya.net>
2025-03-11 11:32:48 +0530

    modified response json

--- routes/lookupRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getOrgTypeController, getIndustryTypeController } = require('../controllers/lookupController');
-const router = express.Router();
-
-router.get('/org-types', getOrgTypeController);
-router.get('/industry-types', getIndustryTypeController);
-
-module.exports = router;
--- utils/jwUtils.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const generateToken = (payload) => {
-    return jwt.sign({id:payload}, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
-
-module.exports = { generateToken};
