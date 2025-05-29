74661d4a9e643e7e865e46bb1a9b38231242412a
Priya Varshney <priya.varshney@agreeya.net>
2025-05-29 12:00:41 +0530

    created api for download deep report

--- controllers/downloadController.js
+++ controllers/downloadController.js
@@ -1,4 +1,4 @@
-const { generateAccessibilityReport, generateManualAccessibilityReport } = require("../services/downloadService");
+const { generateAccessibilityReport, generateManualAccessibilityReport, generateDeepAccessibilityReport } = require("../services/downloadService");
 
 
 exports.generateAccessibilityReportController = async (req, res, next) => {
@@ -20,6 +20,20 @@ exports.generateManualAccessibilityReportController = async (req, res, next) => 
     const { txn_id } = req.params;
 
     const { buffer, filename } = await generateManualAccessibilityReport(txn_id);
+
+    res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+    res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
+    res.send(buffer);
+  } catch (err) {
+    next(err); 
+  }
+};
+
+exports.generateDeepAccessibilityReportController = async (req, res, next) => {
+  try {
+    const { assessment_id, txn_id } = req.query;
+
+    const { buffer, filename } = await generateDeepAccessibilityReport(assessment_id, txn_id);
 
     res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
     res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
--- routes/miscRoutes.js
+++ routes/miscRoutes.js
@@ -8,7 +8,7 @@ const { SuccessReturnHandler } = require('../middlewares/responseHandler');
 const { STATUS_CODES } = require('../utils/errorCodes');
 const { SUCCESS_MESSAGES } = require('../utils/responseMessages');
 const {verifyJwt} = require('../middlewares/auth');
-const { generateAccessibilityReportController, generateManualAccessibilityReportController } = require('../controllers/downloadController');
+const { generateAccessibilityReportController, generateManualAccessibilityReportController, generateDeepAccessibilityReportController } = require('../controllers/downloadController');
 
 
 // Configure multer with better file validation
@@ -148,7 +148,8 @@ res.status(STATUS_CODES.SUCCESS).json(successResponse);
   }
 });
  
-router.get('/download-docx/:assessment_id', generateAccessibilityReportController)
-router.get('/download-manual-docx/:txn_id', generateManualAccessibilityReportController)
+router.get('/download-docx/:assessment_id', generateAccessibilityReportController);
+router.get('/download-manual-docx/:txn_id', generateManualAccessibilityReportController);
+router.get('/download-deep-docx', generateDeepAccessibilityReportController);
 
 module.exports = router;
