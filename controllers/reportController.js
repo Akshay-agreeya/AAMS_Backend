ed9afae654da19ad77b45578a3cb3462e0d3e0cc
Priya Varshney <priya.varshney@agreeya.net>
2025-07-22 12:20:56 +0530

    added view mobile_rule_results API

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,7 +1,7 @@
 const { STATUS_CODES, ERROR_MESSAGES } = require("../utils/errorCodes");
 const {SUCCESS_MESSAGES } = require('../utils/responseMessages');
 const { SuccessReturnHandler } = require("../middlewares/responseHandler");
-const { getWebUrlsService, getAssessmentsService, getCategoryDataService, getUserWebUrlsService, insertCategoryAndDetailsService, updateCategoryAndDetailsService, deleteCategoryReport, getMobileScreenReportService } = require("../services/reportsService");
+const { getWebUrlsService, getAssessmentsService, getCategoryDataService, getUserWebUrlsService, insertCategoryAndDetailsService, updateCategoryAndDetailsService, deleteCategoryReport, getMobileScreenReportService, getMobileruleResultsService } = require("../services/reportsService");
 
 exports.getWebUrlsController = async(req,res,next) =>{
     const {org_id} = req.params;
@@ -152,4 +152,25 @@ exports.updateCategoryAndDetails = async (req, res, next) => {
     }catch(err){
         next(err);
     }
+}
+
+exports.getMobileRuleResultsController = async(req,res,next) =>{
+  const {mobile_screen_report_id} = req.params;
+  const { page, size, status } = req.query;
+  
+  const pageNumber = parseInt(page, 10) || 1;
+  const pageSize = parseInt(size, 10) || 10;
+
+
+  try{
+      const ruleResults = await getMobileruleResultsService(mobile_screen_report_id, status, pageNumber, pageSize);
+
+      const successResponse = SuccessReturnHandler({
+          message : SUCCESS_MESSAGES.DETAILS_FETCHED_SUCCESS,
+          resp: ruleResults,
+      });
+      res.status(STATUS_CODES.SUCCESS).json(successResponse);
+  }catch(err){
+      next(err);
+  }
 }
